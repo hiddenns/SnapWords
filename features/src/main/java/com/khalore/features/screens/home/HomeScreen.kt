501d1fec3c69bfd214a0a766da5d6dbd2375f168
khalym <aerys.prog@gmail.com>
2024-01-17 23:23:17 +0200

    implemented swappable cards component

--- features/src/main/java/com/khalore/features/screens/home/HomeScreen.kt
+++ features/src/main/java/com/khalore/features/screens/home/HomeScreen.kt
@@ -1,20 +1,9 @@
 package com.khalore.features.screens.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import com.khalore.features.components.swipable.cards.SwappableCards
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Home Screen"
-        )
-    }
+    SwappableCards()
 }
